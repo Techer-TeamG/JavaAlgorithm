70a5c01b50a4bba1f94c0cb0f043996a275739e6
Somi <somii009@ewhain.net>
2021-09-17 23:01:07 +0900

    문자열 2941, 4949번

---
+++ Baekjoon/Somi/String/4949.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static String solution(String str) {
+		Stack<Character> stack = new Stack<>();
+	
+		for(int i =0 ;i <str.length(); i++) {
+			char x = str.charAt(i);
+			
+			if(x=='(' || x=='[') stack.push(x);
+			else if(x==')') {
+				if(stack.empty() || stack.pop()!='(') return "no";
+			}
+			else if(x==']') {
+				if(stack.empty() || stack.pop()!='[') return "no";
+			}
+		}
+	
+		if(stack.empty()) return "yes";
+		else return "no";
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			String str = br.readLine();
+			if(str.equals(".")) break;
+			System.out.println(solution(str));
+		}
+	}
+
+}
