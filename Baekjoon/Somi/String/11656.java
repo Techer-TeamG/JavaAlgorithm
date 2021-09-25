34bde069eef8696c708f771fb659ce135ac25fa4
rlathal009 <rlathal009@gmail.com>
2021-09-25 13:31:05 +0900

    문자열 백준 11656번

---
+++ Baekjoon/Somi/String/11656.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static String[] solution(String str) {
+		String[] answer = new String[str.length()];
+		
+		for(int i=0; i<str.length(); i++) {
+			answer[i] = str.substring(i);
+		}
+		
+		Arrays.sort(answer);
+		
+		return answer;
+	}
+
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		for(String x : solution(str)) {
+			System.out.println(x);
+		}
+	}
+}
